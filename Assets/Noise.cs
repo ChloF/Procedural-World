decf32246856af310a9593475f5ab6cc66336fcc
LeoFisher657 <leofisher657@gmail.com>
2019-08-30 21:10:10 +0100

    Created Noise.cs
    Generates perlin noise maps

---
+++ Assets/Noise.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class Noise
+{
+    public static float[,] GenerateNoiseMap(int width, int height, float scale)
+    {
+        float[,] map = new float[width, height];
+
+        for (int y = 0; y < height; y++)
+        {
+            for (int x = 0; x < width; x++)
+            {
+                float sampleX = x / scale;
+                float sampleY = y / scale;
+
+                float noise = Mathf.PerlinNoise(sampleX, sampleY);
+
+                map[x, y] = noise;
+            }
+        }
+
+        return map;
+    }
+}
