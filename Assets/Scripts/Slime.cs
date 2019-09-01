d9f4b98ac1f83d2f893ec90adaf846024223d702
LeoFisher657 <leofisher657@gmail.com>
2019-09-01 15:53:01 +0100

    Added slime "hop"
    OnClick, a slime will hop forward.

---
+++ Assets/Scripts/Slime.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class Slime : MonoBehaviour
+{
+    public float horizontalForce;
+    public float verticalForce;
+    private Rigidbody rb;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    private void Update()
+    {
+        if(Input.GetMouseButtonDown(0))
+        {
+            Hop(transform.forward, horizontalForce, verticalForce);
+        }
+    }
+
+    void Hop(Vector3 direction, float h, float v)
+    {
+        direction.y = 0;
+
+        rb.AddForce(direction.normalized * h + Vector3.up * v, ForceMode.Impulse);
+    }
+}
