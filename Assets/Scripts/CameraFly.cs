fe4fa4ec68131a8a1b335fa326486a0a52349c88
LeoFisher657 <leofisher657@gmail.com>
2019-09-01 17:03:13 +0100

    Update CameraFly.cs

--- Assets/Scripts/CameraFly.cs
+++ Assets/Scripts/CameraFly.cs
@@ -14,6 +14,7 @@ public class CameraFly : MonoBehaviour
 
     private void Start()
     {
+        Cursor.lockState = CursorLockMode.Locked;
         totalRotX = 0;
         transform.rotation = Quaternion.identity;
         rb = GetComponent<Rigidbody>();
