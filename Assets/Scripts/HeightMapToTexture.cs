c044fafa0474db0959d4256291dd6f40b8fbb1e1
LeoFisher657 <leofisher657@gmail.com>
2019-09-02 11:39:39 +0100

    Update HeightMapToTexture.cs
    Texture generates regionmap first

--- Assets/Scripts/HeightMapToTexture.cs
+++ Assets/Scripts/HeightMapToTexture.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public static class HeightMapToTexture
-{
+{ 
     public static Texture2D GenerateTexture(float[,] heightMap, TerrainType[] regions, FilterMode filterMode)
     {
         int width = heightMap.GetLength(0);
@@ -11,6 +11,27 @@ public static class HeightMapToTexture
 
         Color[] colourMap = new Color[(width - 1) * (height - 1)];
 
+        TerrainType[] regionMap = GenerateRegionMap(heightMap, regions);
+
+        for (int i = 0; i < regionMap.Length; i++)
+        {
+            colourMap[i] = regionMap[i].colour;
+        }
+
+        Texture2D tex = new Texture2D(width - 1, height - 1);
+
+        tex.SetPixels(colourMap);
+        tex.filterMode = filterMode;
+
+        return tex;
+    }
+
+    public static TerrainType[] GenerateRegionMap(float[,] heightMap, TerrainType[] regions)
+    {
+        int width = heightMap.GetLength(0);
+        int height = heightMap.GetLength(1);
+
+        TerrainType[] regionMap = new TerrainType[width * height];
         for (int y = 0; y < height - 1; y++)
         {
             for (int x = 0; x < width - 1; x++)
@@ -21,19 +42,13 @@ public static class HeightMapToTexture
                 {
                     if (curHeight <= regions[i].height)
                     {
-                        colourMap[y * (width - 1) + x] = regions[i].colour;
+                        regionMap[y * width + x] = regions[i];
                         break;
                     }
                 }
             }
         }
 
-
-        Texture2D tex = new Texture2D(width - 1, height - 1);
-
-        tex.SetPixels(colourMap);
-        tex.filterMode = filterMode;
-
-        return tex;
+        return regionMap;
     }
 }
