1749c4877d39c17e5af274aaf421ca7e7a608e86
LeoFisher657 <leofisher657@gmail.com>
2019-09-05 14:56:17 +0100

    Update Noise.cs

--- Assets/Scripts/Noise.cs
+++ Assets/Scripts/Noise.cs
@@ -6,7 +6,10 @@ public static class Noise
 {
     public static float[,] GenerateNoiseMap(int width, int height, float scale, int octaves, float persistence, float lacunarity, int seed, Vector2 offset)
     {
+        //Initialise random generator.
         System.Random prng = new System.Random(seed);
+        
+        //Create random offsets to sample each octave from a different region.
         Vector2[] octaveOffsets = new Vector2[octaves];
         for (int i = 0; i < octaves; i++)
         {
@@ -15,9 +18,10 @@ public static class Noise
 
             octaveOffsets[i] = new Vector2(offsetX, offsetY) + offset;
         }
-
+        
         float[,] noiseMap = new float[width, height];
-
+        
+        //Keep track of minimum and maximum for normalisation.
         float min = float.MaxValue;
         float max = float.MinValue;
 
@@ -31,16 +35,20 @@ public static class Noise
              
                 for (int i = 0; i < octaves; i++)
                 {
+                    //Pick a point to sample perlin noise from.
                     float sampleX = (x - width / 2) / scale * frequency + octaveOffsets[i].x;
                     float sampleY = (y - height / 2) / scale * frequency + octaveOffsets[i].y;
-
+                    
+                    //Shift noise between -1/2 and 1/2.
                     float perlinValue = 2 * Mathf.PerlinNoise(sampleX + seed, sampleY + seed) - 1;
                     noiseHeight += perlinValue * amplitude;
-
+                    
+                    //Modify amplitude and frequency for each octave.
                     amplitude *= persistence;
                     frequency *= lacunarity;
                 }
-
+                
+                //Update minimum and maximum.
                 if (noiseHeight < min)
                 {
                     min = noiseHeight;
@@ -54,11 +62,12 @@ public static class Noise
             }
         }
 
-        //Normalize
+        //Normalise.
         for (int y = 0; y < height; y++)
         {
             for (int x = 0; x < width; x++)
             {
+                //InverseLerp returns how far between min and max the noise value is.
                 noiseMap[x, y] = Mathf.InverseLerp(min, max, noiseMap[x, y]);
             }
         }
