a383905c0c6e7f2bb3142a62ba632cbfaa6835bd
LeoFisher657 <leofisher657@gmail.com>
2019-09-05 14:43:16 +0100

    Update CameraFly.cs

--- Assets/Scripts/CameraFly.cs
+++ Assets/Scripts/CameraFly.cs
@@ -7,7 +7,8 @@ public class CameraFly : MonoBehaviour
 {
     public float speed;
     public float rotationSensitivity;
-
+    
+    //Keep track of total rotation for clamping.
     private float totalRotX;
 
     private Rigidbody rb;
@@ -19,17 +20,18 @@ public class CameraFly : MonoBehaviour
         rb = GetComponent<Rigidbody>();
     }
 
-    private void Update()
+    private void FixedUpdate()
     {
         Vector3 force = new Vector3(Input.GetAxis("X"), Input.GetAxis("Y"), Input.GetAxis("Z"));
-        force = transform.TransformDirection(force);
-        force *= speed * Time.deltaTime * 100;
+        force = transform.TransformDirection(force); //Convert force from local space to world space.
+        force *= speed * Time.fixedDeltaTime * 100;
 
         rb.AddForce(force);
 
-        float rotX = -Input.GetAxis("Mouse Y") * rotationSensitivity * Time.deltaTime;
-        float rotY = Input.GetAxis("Mouse X") * rotationSensitivity * Time.deltaTime;
-
+        float rotX = -Input.GetAxis("Mouse Y") * rotationSensitivity * Time.fixedDeltaTime;
+        float rotY = Input.GetAxis("Mouse X") * rotationSensitivity * Time.fixedDeltaTime;
+        
+        //Clamp rotation between 60 degrees and -70 degrees.
         if((totalRotX < 60f || rotX < 0) && (totalRotX > -70f || rotX > 0))
         {
             totalRotX += rotX;
