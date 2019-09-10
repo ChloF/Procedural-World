ee5ac9302c703edd682b20d67eea6ca2b88d6bf3
LeoFisher657 <leofisher657@gmail.com>
2019-09-10 09:24:30 +0100

    Update FoodSpawner.cs

--- Assets/Scripts/FoodSpawner.cs
+++ Assets/Scripts/FoodSpawner.cs
@@ -7,8 +7,9 @@ public class FoodSpawner : MonoBehaviour
     public int tickRate;
     public GameObject[] foodTypes;
     public float[] spawnRates;
-    public int maxSpawnPointTrials;
     public int maxFood;
+    public int maxSpawnPointTrials;
+    public bool spawning;
     public float xMin;
     public float xMax;
     public float yMin;
@@ -23,23 +24,29 @@ public class FoodSpawner : MonoBehaviour
     {
         while(true)
         {
-            if (Food.allFood.Count < maxFood)
+			//Check if the spawner is enabled, and if it has already exceeded the max amont of food.
+            if(spawning && Food.allFood.Count < maxFood)
             {
-                for (int i = 0; i < foodTypes.Length; i++)
-                {
-                    if (Random.value < spawnRates[i] / tickRate)
-                    {
-                        SpawnFood(foodTypes[i]);
-                    }
-                }
+				//Iterate over each food type.
+				for (int i = 0; i < foodTypes.Length; i++)
+				{
+					//Randomly choose whether to spawn, so the rate per second is equal to the corresponding spawnrate.
+					if (Random.value < spawnRates[i] / tickRate)
+					{
+						SpawnFood(foodTypes[i]);
+					}
+				}
             }
-
+			//Run the coroutine tickRate times per second.	
             yield return new WaitForSeconds(1 / tickRate);
         }
+		
+		yield return null;
     }
 
     private void SpawnFood(GameObject food)
     {
+		//Instantiate the food at a specific spawnPoint.
         Food f = food.GetComponent<Food>();
         Vector3 p = SpawnPoint(f.size / 2, f.minHeight, f.maxHeight);
             
@@ -48,19 +55,24 @@ public class FoodSpawner : MonoBehaviour
 
     private Vector3 SpawnPoint(float heightOffset, float minHeight, float maxHeight)
     {
-        int i = 0;
-        while (true)
+        //Only try to find a spawnpoint a fixed number of times.
+        for(int i = 0; i < maxSpawnPointTrials)
         {
+            //Pick random x and y values.
             float tx = Random.value;
             float ty = Random.value;
-
+            
+            //Interpolate between the maximum and minimum.
             float x = Mathf.Lerp(xMin, xMax, tx);
             float y = Mathf.Lerp(yMin, yMax, ty);
-
+            
+            //Try and create a spawnPoint
             Vector3 spawnPoint = new Vector3(x, 100, y);
 
             Ray ray = new Ray(spawnPoint, Vector3.down);
-
+            
+            //If there is land under the spawnPoint, and that land is within the height range for that food,
+            //set the spawnPoint to be at that point on the ground.
             if (Physics.Raycast(ray, out RaycastHit hit, float.MaxValue))
             {
                 if (hit.point.y >= minHeight && hit.point.y <= maxHeight)
@@ -68,14 +80,10 @@ public class FoodSpawner : MonoBehaviour
                     return hit.point + Vector3.up * heightOffset;
                 }
             }
-
-            i++;
-
-            if(i > 50)
-            {
-                Debug.LogError("Could not find a valid food spawn point.");
-                return Vector3.zero;
-            }
         }
+        
+        //If no spawn point can be found in a reasonable number of trials, give a warning.
+        Debug.LogWarning("Could not find a valid food spawn point.");
+        return Vector3.zero;
     }
 }
